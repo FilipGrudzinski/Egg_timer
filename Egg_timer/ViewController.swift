4b000873907e46d5d2c2d32078249e895624da86
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-04-16 21:57:51 +0200

    Timer_app

--- Egg_timer/ViewController.swift
+++ Egg_timer/ViewController.swift
@@ -10,9 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    var seconds: Int = 210
+
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        
+        timer.invalidate()
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.pressedTimer), userInfo: nil, repeats: true)
+
+    }
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @objc func pressedTimer() {
+        
+        timerLabel.text =  String(seconds) + (" seconds")
+        seconds -= 1
+        if seconds == 0 {
+            
+            timer.invalidate()
+            
+            timerLabel.text =  ("0")
+        }
+        
+    }
+    
+    @IBAction func addTimerButton(_ sender: Any) {
+        
+        seconds += 10
+        timerLabel.text =  String(seconds) + (" seconds")
+        
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        
+        seconds = 210
+        timerLabel.text =  String(seconds) + (" seconds")
+    }
+    
+    
+    @IBAction func lessTimeButton(_ sender: Any) {
+        
+        if seconds > 10 {
+                seconds -= 10
+                timerLabel.text =  String(seconds) + (" seconds")
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
